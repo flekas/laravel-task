4b7be3bd89d07402ff255fd46e5e6d7875947a26
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-19 14:03:30 +0200

    publish way/generator

--- app/config/development/app.php
+++ app/config/development/app.php
@@ -54,6 +54,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
