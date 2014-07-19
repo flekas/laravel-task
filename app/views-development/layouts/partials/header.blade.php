77b87e5d82df4d007a18a8d345b3c73c99e80e3e
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-19 15:02:49 +0200

    little css for completed and uncompleted li items

--- app/views-development/layouts/partials/header.blade.php
+++ app/views-development/layouts/partials/header.blade.php
@@ -3,5 +3,10 @@
 	<head>
 		<meta charset="utf-8">
 		{{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
+		<style>
+			li img{margin-right:1em;}
+			.completed{background:#8acc6e;}
+			.uncompleted{background:#FFACAC;}
+		</style>
 	</head>
 	<body>
