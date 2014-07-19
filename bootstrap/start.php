1209f7d5710dee4c0a6c8d9ef40bcbe220a7c3a0
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-19 12:48:10 +0200

    set env detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -23,12 +23,11 @@ $app = new Illuminate\Foundation\Application;
 | given environment, then we will automatically detect it for you.
 |
 */
-
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+//putenv("ENV=production");
+$env = $app->detectEnvironment(function ()
+{
+	return getenv( "ENV" ) ?: 'development';
+});
 
 /*
 |--------------------------------------------------------------------------
