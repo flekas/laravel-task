f4327aba7f3689b45359d4653ac4fa993a64353c
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-20 13:32:25 +0200

    implement UserCreatorService into users controller

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,12 +1,36 @@
 <?php
 
+use Acme\Services\UserCreatorService;
+use Acme\Validators\ValidationException;
+
 class UsersController extends \BaseController {
+
+	protected $userCreator;
+
+	public function __construct(UserCreatorService $userCreator)
+	{
+		$this->userCreator = $userCreator;
+	}
 
 	public function index()
 	{
 		$users = User::all();
 
 		return View::make('users.index', compact('users'));
+	}
+
+	public function store()
+	{
+		try
+		{
+			$this->userCreator->make(Input::all());
+		}
+		catch(ValidationException $e)
+		{
+			return Redirect::back()->withInput()->withErrors($e->getErrors());
+		}
+
+		return Redirect::to('users');
 	}
 
 }
