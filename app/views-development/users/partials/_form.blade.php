432a4143f1b439783201bc97d2a179f90750afb4
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-20 13:38:42 +0200

    fix user create form

--- app/views-development/users/partials/_form.blade.php
+++ app/views-development/users/partials/_form.blade.php
@@ -1,6 +1,6 @@
 {{ Form::open(['url' => '/users', 'class' => 'form']) }}
 	<div class="form-group">
-		{{ Form::label('username', 'Title: ') }}
+		{{ Form::label('username', 'Username: ') }}
 		{{ Form::text('username', null, ['class' => 'form-control']) }}
 		{{ $errors->first('username', '<span class="error">:message</span>') }}
 	</div>
@@ -10,8 +10,8 @@
 		{{ $errors->first('email', '<span class="error">:message</span>') }}
 	</div>
 	<div class="form-group">
-		{{ Form::label('password', 'Body: ') }}
-		{{ Form::text('password', null, ['class' => 'form-control']) }}
+		{{ Form::label('password', 'Password: ') }}
+		{{ Form::password('password', ['class' => 'form-control']) }}
 		{{ $errors->first('password', '<span class="error">:message</span>') }}
 	</div>
 	<div class="form-group">
