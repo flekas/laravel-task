22231cd8902c39f25916e187847c5be22541cab6
Bojan Babic <bojan.babic@sattrakt.com>
2014-07-20 13:37:21 +0200

    include jquery, bootstrap css and js, and create navbar

--- app/views-development/layouts/partials/header.blade.php
+++ app/views-development/layouts/partials/header.blade.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		{{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
+		{{ HTML::script('//code.jquery.com/jquery-1.11.0.min.js') }}
+		{{ HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js') }}
 		<style>
 			.error{color:red;font-style:italic;font-size:.8em;}
 			li img{margin-right:1em;}
@@ -13,3 +15,25 @@
 		</style>
 	</head>
 	<body>
+<nav class="navbar navbar-inverse" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Laravel Task</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="@if(Request::segment(1) == 'users') active @endif"><a href="/users">Users</a></li>
+        <li class="@if(Request::segment(1) == 'tasks') active @endif"><a href="/tasks">Tasks</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
